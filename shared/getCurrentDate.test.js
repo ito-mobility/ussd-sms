a2cde56cf7cda00470fff668c038054538843b77
BAHATI Robben <bahatiroben@gmail.com>
2021-08-05 09:05:29 +0200

    updating the changelog

--- shared/getCurrentDate.test.js
+++ shared/getCurrentDate.test.js
@@ -2,6 +2,8 @@ const getCurrentDate = require('./getCurrentDate');
 
 describe('current current date', () => {
     it('should return the current date', () => {
-        expect(getCurrentDate()).toEqual(new Date());
+        const returnedDate = getCurrentDate();
+        const currentDate = new Date();
+        expect((returnedDate.toISOString()).split('T')[0]).toEqual((currentDate.toISOString()).split('T')[0]);
     });
 });

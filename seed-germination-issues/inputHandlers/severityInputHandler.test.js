0fe4c71f445ffcf08b467c58e50e59e36e02db13
BAHATI Robben <bahatiroben@gmail.com>
2021-08-23 14:12:20 +0200

    updating the severity issues menu

--- seed-germination-issues/inputHandlers/severityInputHandler.test.js
+++ seed-germination-issues/inputHandlers/severityInputHandler.test.js
@@ -2,14 +2,15 @@ const phoneNumberInputHandler = require('./phoneNumberInputHandler');
 const severityInputHandler = require('./severityInputHandler');
 
 describe('issue severity', () => {
+    beforeAll(() => {
+        state.vars.severity_screens = JSON.stringify({1: 'screen 1', 2: 'screen 2'});
+        state.vars.severity_options = JSON.stringify({1: '0-25%', 2: '26-50%', 3: '51-75%', 4: '76-100%'});
+        state.vars.current_severity_screen = '1';
+    });
     it('should reprompt for severity if the user enters an invalid input', () => {
         var handler = severityInputHandler.getHandler('en-ke');
         handler('9');
-        expect(sayText).toHaveBeenCalledWith('What is the severity of the germination issue?\n' +
-        '1. Low (0-25% crops did not germinate)' +
-        '2. Medium(26-50%  crops did not germinate)\n' +
-        '3. High(51-75% crops did not germinate)\n' +
-        '4. Very high(76-100% crops did not germinate)');
+        expect(sayText).toHaveBeenCalledWith('screen 1');
         expect(promptDigits).toHaveBeenCalledWith(severityInputHandler.handlerName);
     });
 
@@ -19,5 +20,14 @@ describe('issue severity', () => {
         expect(state.vars.issues_severity);
         expect(sayText).toHaveBeenCalledWith('Please provide your phone number so we can follow up with you');
         expect(promptDigits).toHaveBeenCalledWith(phoneNumberInputHandler.handlerName);
+    });
+
+    it('should prompt show the next screen and reprompt for 77 if a client enters 77', () => {
+        var handler = severityInputHandler.getHandler('en-ke');
+        handler('77');
+        expect(state.vars.issues_severity);
+        expect(sayText).toHaveBeenCalledWith('screen 2');
+        expect(promptDigits).toHaveBeenCalledWith(severityInputHandler.handlerName);
+        expect(state.vars.current_severity_screen).toEqual(2);
     });
 });
